4854af8391ac9b4f8687732c39907be6ca4941ae
ksw6169 <ksw6169@naver.com>
2021-10-31 23:11:56 +0900

    Swagger 설정, Gradle Encoding 설정

---
+++ src/main/java/com/tancheon/account/config/OpenApiConfiguration.java
@@ -0,0 +1,23 @@
+package com.tancheon.account.config;
+
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springdoc.core.SpringDocConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Profile(value = { "local", "dev", "stg" })
+    @Bean
+    public SpringDocConfiguration springDocConfiguration() {
+        return new SpringDocConfiguration();
+    }
+
+    @Profile(value = { "local", "dev", "stg" })
+    @Bean
+    public SpringDocConfigProperties springDocConfigProperties() {
+        return new SpringDocConfigProperties();
+    }
+}
